9e323cab05ea5ce1128decd691e283e8c8ef9551
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-23 10:55:02 +0530

    Add digonal without variable pattern

--- All-Patterns/number-patterns/diagonalNumbers.cpp
+++ All-Patterns/number-patterns/diagonalNumbers.cpp
@@ -17,6 +17,7 @@ int main () {
     cout << "Enter Heigth:";
     cin >> n;
 
+    cout << endl << "Normal While Loop With variable" << endl;
     int row = 1;
     while ( row <= n ){ 
         int count = row;
@@ -28,6 +29,19 @@ int main () {
         }
         cout << endl;
         row = row + 1;
+    }
+
+    cout << endl << "While Loop WithOut Extra variable" << endl;
+
+    int row2 = 1;
+    while ( row2 <= n ){
+        int col2 = 1;
+        while( col2 <= row2 ) {
+            cout << row2 + col2 - 1 << ' ';
+            col2 = col2 + 1;
+        }
+        cout << endl;
+        row2 = row2 + 1;
     }
 }
 
