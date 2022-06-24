c394e9ab29b44120e03a4b86bf956de230a9cf77
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-24 10:42:30 +0530

    Left and Right Sift

---
+++ operators/leftShiftAndRigthShift.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+/**
+ * Left shift (<<)
+ * Right shift (>>)
+*/
+
+int main () {
+    cout << "Left Shift (* 2):"<<endl;
+    cout << "(2<<1):" << (2 << 1) << endl;
+    cout << "(2<<2):" << (2 << 2) << endl;
+    cout << "(15<<2):" << (15 << 2) << endl;
+
+    cout << "Right Shift (/2):"<< endl;
+    cout << "(24 >> 1):" << (24 >> 1) << endl;
+    cout << "(24 >> 2):" << (24 >> 2) << endl;
+    cout << "(21 >> 2):" << (21 >> 2) << endl;
+}
