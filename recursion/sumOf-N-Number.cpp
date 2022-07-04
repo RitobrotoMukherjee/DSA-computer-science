f0f8f5ee58a8ecfe201be0fbfe9b1daa30c65ec0
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-04 10:14:37 +0530

    Sum of n number by adding using recursion

--- recursion/sumOf-N-Number.cpp
+++ recursion/sumOf-N-Number.cpp
@@ -1,17 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sumOfN(int n){
-    if(n <= 1) {
+int sumOfNBySubstracting(int n)
+{
+    if (n <= 1)
+    {
         return 1;
     }
-    return n + sumOfN(n - 1);
+    return n + sumOfNBySubstracting(n - 1);
 };
 
-int main() {
+int sumOfNByAdding(int n, int times = 1)
+{
+    /**
+     * > take variable to start from 1;
+     * > most case time == n return times;
+     * > return times + sumOfNByAdding(n, times + 1);
+     */
+    if( times == n) {
+        return times;
+    }
+    return times + sumOfNByAdding(n, times + 1);
+}
+
+int main()
+{
     int n;
     cout << "Enter the number you want to sum till: ";
     cin >> n;
 
-    cout << sumOfN(n);
+    cout << sumOfNBySubstracting(n);
+    cout << endl
+         << endl;
+    cout << sumOfNByAdding(n);
 }
