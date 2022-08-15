0af4375f46fc3eb418f8bdd43d716d95f4bdd8cd
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-08-15 14:13:03 +0530

    Implement tree using recursive approch and Linked List

---
+++ Tree-Binary-Search-Treee/CreateTree.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* createTree(node* root) {
+    cout << "Enter data to insert in node: ";
+
+    int data;
+    cin >> data;
+
+    root = new node(data);
+
+    if(data < 0) {
+        return NULL;
+    }
+
+    cout << "Enter Data To Insert In Left Of " << data << " : " << endl;
+    root->left = createTree(root->left);
+    cout << "Enter Data To Insert In Right Of " << data << " : " << endl;
+    root->right = createTree(root->right);
+    return root;
+};
+
+int main () {
+    node* root = NULL;
+
+    root = createTree(root);
+}
